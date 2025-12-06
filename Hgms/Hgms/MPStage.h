373a88d8f5fe863923c4dff2dac3561b910f33ea
Matthew Wong <matt8wong@gmail.com>
2025-12-06 14:20:38 -0800

    Changed the anonymous namespace to private helper functions

--- Hgms/Hgms/MPStage.h
+++ Hgms/Hgms/MPStage.h
@@ -3,8 +3,8 @@
 // Multi-plane stage header file for representing multi-plane GMS processing stage. 
 // This class derives from ProcessingStage and provides implementation for multi-plane
 // GMS feature match filtering. This class supports the following operations:
-//	1. 
-// Authors:  Matthew Wong, Brennan O’Reilly, Pranshu Bhardwaj
+//  1. 
+// Authors:  Matthew Wong, Brennan Oâ€™Reilly, Pranshu Bhardwaj
 //---------------------------------------------------------------------------
 // Inputs:
 //  -- 
@@ -22,38 +22,58 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <opencv2/core.hpp>
 #include "ProcessingStage.h"
 
 class MPStage : public ProcessingStage {
 public:
-	/*----------------------------- default -----------------------------------
-	* Default constructor for class MPStage.
-	* Preconditions: none
-	* Postconditions: Instance of MPStage class is instantiated
-	*/
-	MPStage();
+    /*----------------------------- default -----------------------------------
+    * Default constructor for class MPStage.
+    * Preconditions: none
+    * Postconditions: Instance of MPStage class is instantiated
+    */
+    MPStage();
 
-	/*----------------------------- destructor -------------------------------
-	* Destructor for class MPStage.
-	* Preconditions: Instance of MPStage class is instantiated.
-	* Postconditions: Objects are deallocated.
-	*/
-	virtual ~MPStage();
+    /*----------------------------- destructor -------------------------------
+    * Destructor for class MPStage.
+    * Preconditions: Instance of MPStage class is instantiated.
+    * Postconditions: Objects are deallocated.
+    */
+    virtual ~MPStage();
 
-	/*----------------------------- execute -------------------------------
-	* Execute method for class MPStage.
-	* Preconditions: Instance of MPStage class is instantiated and image 1
-	*				 and 2 keypoints are provided, image1 and 2 sizes are provided,
-	*                image 1 and 2 feature matches are provided, thresholdFactor
-	*                is provided.
-	* Postconditions: The execute method is run and algorithm executed
-	*/
-	virtual void execute(const std::vector<KeyPoint>& vkp1, const Size& size1,
-		const std::vector<KeyPoint>& vkp2, const Size& size2,
-		const std::vector<DMatch>& matchesAll, std::vector<DMatch>& vDMatches,
-		const double thresholdFactor);
+    /*----------------------------- execute -------------------------------
+    * Execute method for class MPStage.
+    * Preconditions: Instance of MPStage class is instantiated and image 1
+    *                and 2 keypoints are provided, image1 and 2 sizes are provided,
+    *                image 1 and 2 feature matches are provided, thresholdFactor
+    *                is provided.
+    * Postconditions: The execute method is run and algorithm executed
+    */
+    virtual void execute(const std::vector<cv::KeyPoint>& vkp1, const cv::Size& size1,
+        const std::vector<cv::KeyPoint>& vkp2, const cv::Size& size2,
+        const std::vector<cv::DMatch>& matchesAll, std::vector<cv::DMatch>& vDMatches,
+        const double thresholdFactor);
 
 private:
-	// class identifier 
-	static constexpr const char* STAGE_NAME = "MP";
+    // class identifier 
+    static constexpr const char* STAGE_NAME = "MP";
+
+    // ---------------------- helper functions ----------------------
+
+    static std::vector<cv::Point2f> computeMotionVectors(
+        const std::vector<cv::KeyPoint>& vkp1,
+        const std::vector<cv::KeyPoint>& vkp2,
+        const std::vector<cv::DMatch>& matches);
+
+    static std::vector<int> clusterMotionVectors(
+        const std::vector<cv::Point2f>& motion,
+        const int K);
+
+    static void filterPlaneMatches(
+        const std::vector<cv::DMatch>& planeMatches,
+        const std::vector<cv::KeyPoint>& vkp1,
+        const std::vector<cv::KeyPoint>& vkp2,
+        const double thresholdFactor,
+        std::vector<cv::DMatch>& inliersOut);
 };
